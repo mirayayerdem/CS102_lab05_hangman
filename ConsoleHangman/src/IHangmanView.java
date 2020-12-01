a3ac1a44967f8b02d92e2fcfbd468e10afb71d31
miray <mirayayerdem@gmail.com>
2020-12-01 18:17:53 +0300

    IHangmanView Class is created

---
+++ ConsoleHangman/src/IHangmanView.java
@@ -0,0 +1,13 @@
+package ConsoleHangman.src;
+
+import cs102.Hangman;
+/**
+ * IHangmanView - view interface for MVC demo using Hangman
+ *
+ * @author David
+ * @version 1.00 2013/4/7
+ */
+public interface IHangmanView
+{
+	public void updateView( Hangman hangmanModel);
+}
